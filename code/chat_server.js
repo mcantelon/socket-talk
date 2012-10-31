ef226baf33de21b22b2faecae2c022d2da643d6a
Mike Cantelon <mcantelon@gmail.com>
2012-10-30 21:53:49 -0700

    Stuff

---
+++ code/chat_server.js
@@ -0,0 +1,29 @@
+var net = require('net')
+  , clients = {};
+
+var server = net.createServer(function(conn) {
+  var id = conn.remotePort;
+  conn.on('connect', function() {
+    clients[id] = conn;
+  });
+  conn.on('data', function(data) {
+    broadcast(id, data);
+  });
+  conn.on('end', function(data) {
+    delete clients[id];
+  });
+});
+
+server.listen(3000);
+
+function broadcast(fromId, message) {
+  // if a message, not a control key, has been sent, broadcast it
+  if (message.toString().slice(-1) == "\n") {
+    // send to each client except sender
+    for(var id in clients) {
+      if (fromId != id) {
+        clients[id].write(fromId + '> ' + message);
+      }
+    }
+  }
+}
---
+++ code/hello_web.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Web!\n');
+}).listen(8888);
